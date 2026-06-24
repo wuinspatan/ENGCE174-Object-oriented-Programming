48b9d6809ccbae751869d78766176e21b5fa9a05
wuinspatan <kiritoxoly@gmail.com>
2026-06-24 09:45:37 +0700

    addjust score range condition

--- L1.6/Main.java
+++ L1.6/Main.java
@@ -6,16 +6,18 @@ public class Main {
         int score = sc.nextInt();
         String grade;
 
-        if (score >= 80) {
+        if (score >= 80 && score <= 100) {
             grade = "A";
-        } else if (score >= 70) {   
+        } else if (score >= 70 && score < 80) {   
             grade = "B";
-        } else if (score >= 60) {
+        } else if (score >= 60 && score < 70) {
             grade = "C";
-        } else if (score >= 50) {
+        } else if (score >= 50 && score < 60) {
             grade = "D";
-        } else {
+        } else if (score >= 0 && score < 50) {
             grade = "F";
+        } else {
+            grade = "Score in range 1 - 100!";
         }
         System.out.println("Grade = " + grade);
     }
