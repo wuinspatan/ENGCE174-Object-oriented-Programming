1fd55c0e74cd01ed0cb122a2dead7ec8fc7912e3
wuinspatan <kiritoxoly@gmail.com>
2026-06-24 08:47:18 +0700

    Finish Lab1.3

---
+++ L1.3/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt();
+        int height = sc.nextInt();
+        int area = width * height;
+        int perimeter = 2 * (width + height);
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
