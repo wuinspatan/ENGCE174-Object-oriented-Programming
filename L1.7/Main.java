d863026a0c3f5956a36a2424710f9d49b8de63c9
wuinspatan <kiritoxoly@gmail.com>
2026-06-24 11:04:17 +0700

    Finish Lab1.7 : switch case new rule switch

--- L1.7/Main.java
+++ L1.7/Main.java
@@ -5,20 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int menu = sc.nextInt();
         switch (menu) {
-            case 1:
-                System.out.println("Water");
-                break;
-            case 2:
-                System.out.println("Coffee");
-                break;
-            case 3:
-                System.out.println("Tea");
-                break;
-            case 4:
-                System.out.println("Milk");
-                break;
-            default:
-                System.out.println("Invalid menu");
+            case 1 -> System.out.println("Water");
+            case 2 -> System.out.println("Coffee");
+            case 3 -> System.out.println("Tea");
+            case 4 -> System.out.println("Milk");
+            default -> System.out.println("Invalid menu");
         }
     }
 }
