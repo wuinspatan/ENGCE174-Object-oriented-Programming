021d4e47853b3ceb03dfd240e29de364c4f7248c
wuinspatan <kiritoxoly@gmail.com>
2026-06-30 18:51:40 +0700

    add: L1.12 submission file into WEEK01

---
+++ WEEK01/67543206095-1_ENGCE174_lab_1_12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hours = sc.nextInt();
+        int fee;
+
+        if (hours > 6) {
+            fee = 100;
+        } else if (hours == 1) {
+            fee = 20;
+        } else {
+            fee = (hours * 10) + 10;
+        }
+
+        System.out.println("Fee = " + fee);
+    }
+}
