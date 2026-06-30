255122b5e5518fbe3ad06f30b6a2b04421c06c3e
wuinspatan <kiritoxoly@gmail.com>
2026-06-30 18:51:40 +0700

    add: L1.19 submission file into WEEK01

---
+++ WEEK01/67543206095-1_ENGCE174_lab_1_19.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int weight_pc = sc.nextInt();
+        int type_pc = sc.nextInt();
+        int total_cost;
+
+        if (weight_pc <= 5) {
+            total_cost = 40;
+        } else if (weight_pc > 5 && weight_pc <= 10) {
+            total_cost = 70;
+        } else {
+            total_cost = 100;
+        }
+
+        switch (type_pc) {
+            case 1 -> total_cost -= 20;
+            case 2 -> total_cost -= 10;
+            case 3 -> total_cost -= 0;
+            default -> System.out.println("Invalid");
+        }
+        System.out.println("Shipping = " + total_cost);
+    }
+}
