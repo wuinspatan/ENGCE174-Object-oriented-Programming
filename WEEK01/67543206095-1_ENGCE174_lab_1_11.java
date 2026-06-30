5d845da184bed8bb935d20afd560a2cfa6b3fc3c
wuinspatan <kiritoxoly@gmail.com>
2026-06-30 18:51:40 +0700

    add: L1.11 submission file into WEEK01

---
+++ WEEK01/67543206095-1_ENGCE174_lab_1_11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int units = sc.nextInt();
+        int bill;
+
+        if (units >= 0 && units <= 100) {
+            bill = units * 4;
+        } else if (units >= 101 && units <= 200) {
+            units -= 100;
+            bill = units * 5 + 400;
+        } else { 
+            units -= 200;
+            bill = (units * 6) + 900;
+        }
+
+        System.out.println("Bill = " + bill);
+    }
+}
