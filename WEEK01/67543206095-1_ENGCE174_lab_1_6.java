90a60d9d9eb1e9fb2c23f84b6fd66c60f96bbbcc
wuinspatan <kiritoxoly@gmail.com>
2026-06-30 18:51:40 +0700

    add: L1.6 submission file into WEEK01

---
+++ WEEK01/67543206095-1_ENGCE174_lab_1_6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        String grade;
+
+        if (score >= 80 && score <= 100) {
+            grade = "A";
+        } else if (score >= 70 && score < 80) {   
+            grade = "B";
+        } else if (score >= 60 && score < 70) {
+            grade = "C";
+        } else if (score >= 50 && score < 60) {
+            grade = "D";
+        } else if (score >= 0 && score < 50) {
+            grade = "F";
+        } else {
+            grade = "Score out of range!";
+        }
+        System.out.println("Grade = " + grade);
+    }
+}
