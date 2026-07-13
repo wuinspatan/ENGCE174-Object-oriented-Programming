9a1195265d123e1441582f2962a68ccf0ca3d491
wuinspatan <kiritoxoly@gmail.com>
2026-07-13 22:12:41 +0700

    Add Lab 3.14 solution

---
+++ WEEK03/67543206095-1_ENGCE174_lab_3_14.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println("Perfect");
+        } else {
+            System.out.println("Not Perfect");
+        }
+    }
+
+    
+}
