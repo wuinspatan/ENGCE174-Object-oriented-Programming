5244ffafeede0f667e36ac3a98da845880a90ce2
wuinspatan <kiritoxoly@gmail.com>
2026-08-14 17:26:21 +0700

    no space at last index

--- WEEK07/67543206095-1_ENGCE174_lab_7_1.java
+++ WEEK07/67543206095-1_ENGCE174_lab_7_1.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        // อ่านค่าเข้า Array 2x2
         Scanner sc = new Scanner(System.in);
         int[][] grid = new int[2][2];
+
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 2; col++) {
                 grid[row][col] = sc.nextInt();
@@ -11,9 +13,14 @@ class Main {
         }
         sc.close();
 
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 2; col++) {
-                System.out.print(grid[row][col] + " ");
+        // Display array using nested loop
+        for(int row = 0 ; row < 2 ; row++) {
+            for(int col = 0; col < 2 ; col++) {
+                System.out.print(grid[row][col]);
+                // เว้นวรรคเฉพาะตอนที่ยังไม่ใช่ตัวสุดท้ายของบรรทัด 
+                if (col < 1) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
