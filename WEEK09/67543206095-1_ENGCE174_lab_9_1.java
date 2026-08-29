99cf6e43f2b911eb76b9c8f8afb832ee1e444959
wuinspatan <kiritoxoly@gmail.com>
2026-08-29 14:01:03 +0700

    class student

---
+++ WEEK09/67543206095-1_ENGCE174_lab_9_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Student {
+    String name;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        Student obj = new Student();
+        obj.name = name;
+        System.out.print(obj.name);
+
+        sc.close();
+    }
+}
